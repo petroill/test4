ac88269c0ec5803d41a8c920517cdafc7fa75133
Dan Patrascan <petroill@yahoo.com>
2023-01-16 09:59:42 -0600

    Testing matrix

--- python.py
+++ python.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 from datetime import datetime
 
-print("Hello World")
+print("f'${Greeting} from ${Country}'")
 print(datetime.now())
 
 command = subprocess.run(['ls'], stdout=subprocess.PIPE).stdout.decode('utf-8')
